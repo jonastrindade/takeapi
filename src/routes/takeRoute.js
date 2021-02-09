ac63783d3f0207c5671b89cbaf3763adcb83f66b
jonastrindade <jonastrinda@gmail.com>
2021-02-09 01:40:49 -0300

    chore: manipulate data from json function

--- src/routes/takeRoute.js
+++ src/routes/takeRoute.js
@@ -27,12 +27,13 @@ request.get({
     }
 
     var dataJson = data;
-    console.log(dataJson);
+    // console.log(dataJson);
 
-    multiplyES6(dataJson)
-    
+    const dataSend = manipulateData(dataJson)
 
-    fs.writeFile("./src/data/takeData.json", JSON.stringify(data), function(err){
+    // console.log(dataSend); 
+
+    fs.writeFile("./src/data/takeData.json", JSON.stringify(dataSend), function(err){
         if (err) {
             console.log(err);
         } else {
@@ -41,15 +42,123 @@ request.get({
     });
 });
 
-const multiplyES6 = (data) => {
+const manipulateData = (data) => {
 
-    fiveRepos = []
-
+    var repositories = [];
     data.forEach(element => {
-        {
-            
-        }
+        repositories.push({
+            "header": {
+                "type": "application/vnd.lime.media-link+json",
+                "value": {
+                    "title": element.name,
+                    "text": element.description,
+                    "type": "image/png",
+                    "uri": "https://avatars.githubusercontent.com/u/4369522?s=200&v=4"
+                }
+            }
+        
+        });
     });
+    // console.log(repositories);
+
+    var fiveRepositories = {
+        "itemType": "application/vnd.lime.document-select+json",
+        "items": JSON.stringify(repositories)
+    }
+
+    console.log(fiveRepositories)
+
+    return fiveRepositories;
 };
 
 module.exports = router;
+
+// {
+//     "itemType": "application/vnd.lime.document-select+json",
+//     "items": [
+//         {
+//             "header": {
+//                 "type": "application/vnd.lime.media-link+json",
+//                 "value": {
+//                     "title": "Title",
+//                     "text": "This is a first item",
+//                     "type": "image/jpeg",
+//                     "uri": "http://www.isharearena.com/wp-content/uploads/2012/12/wallpaper-281049.jpg"
+//                 }
+//             },
+//             "options": [
+//                 {
+//                     "label": {
+//                         "type": "application/vnd.lime.web-link+json",
+//                         "value": {
+//                             "title": "Link",
+//                             "uri": "http://www.adoteumgatinho.org.br"
+//                         }
+//                     }
+//                 },
+//                 {
+//                     "label": {
+//                         "type": "text/plain",
+//                         "value": "Text 1"
+//                     },
+//                     "value": {
+//                         "type": "application/json",
+//                         "value": {
+//                             "key1": "value1",
+//                             "key2": "2"
+//                         }
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             "header": {
+//                 "type": "application/vnd.lime.media-link+json",
+//                 "value": {
+//                     "title": "Title 2",
+//                     "text": "This is another item",
+//                     "type": "image/jpeg",
+//                     "uri": "http://www.freedigitalphotos.net/images/img/homepage/87357.jpg"
+//                 }
+//             },
+//             "options": [
+//                 {
+//                     "label": {
+//                         "type": "application/vnd.lime.web-link+json",
+//                         "value": {
+//                             "title": "Second link",
+//                             "text": "Weblink",
+//                             "uri": "https://pt.dreamstime.com/foto-de-stock-brinquedo-pl%C3%A1stico-amarelo-do-pato-image44982058"
+//                         }
+//                     }
+//                 },
+//                 {
+//                     "label": {
+//                         "type": "text/plain",
+//                         "value": "Second text"
+//                     },
+//                     "value": {
+//                         "type": "application/json",
+//                         "value": {
+//                             "key3": "value3",
+//                             "key4": "4"
+//                         }
+//                     }
+//                 },
+//                 {
+//                     "label": {
+//                         "type": "text/plain",
+//                         "value": "More one text"
+//                     },
+//                     "value": {
+//                         "type": "application/json",
+//                         "value": {
+//                             "key5": "value5",
+//                             "key6": "6"
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     ]
+//   }
